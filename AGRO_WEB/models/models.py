463173ca277b1abd54af12b2ec7b5750fcb8df3a
Yeison Ascanio <ydascanioa@ufpso.edu.co>
2023-05-15 16:46:41 -0500

    LLenamos la base de datos de pais, departamento y ciudad

--- AGRO_WEB/models/models.py
+++ AGRO_WEB/models/models.py
@@ -1,9 +1,41 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Country(db.Model):
+    __tablename__ = "country"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=False, nullable=False)
+    zip_code = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return '<Country {}>'.format(self.id)
+    
+class Department(db.Model):
+    __tablename__ = "department"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=False, nullable=False)
+    zip_code = db.Column(db.String(50), unique=True, nullable=False)
+    fk_country = db.Column(db.Integer, ForeignKey('country.id'), unique=False, nullable=False)
+    country = relationship(Country)
+
+    def __repr__(self):
+        return '<Department {}>'.format(self.id)
+    
+class City(db.Model):
+    __tablename__ = "city"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=False, nullable=False)
+    zip_code = db.Column(db.String(50), unique=False, nullable=False)
+    fk_department = db.Column(db.Integer, ForeignKey('department.id'), unique=False, nullable=False)
+    department = relationship(Department)
+    def __repr__(self):
+        return '<City {}>'.format(self.id)
+
+class Farmer(db.Model):
     __tablename__ = "farmer"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=False, nullable=False)
@@ -16,6 +48,12 @@ class User(db.Model):
     genre =  db.Column(db.String(250), nullable=False)
     phone = db.Column(db.String(10), nullable=False)
     address = db.Column(db.String(600), unique=False, nullable=False)
+    fk_country = db.Column(db.Integer, ForeignKey('country.id'), unique=False, nullable=False)
+    country = relationship(Country)
+    fk_department = db.Column(db.Integer, ForeignKey('department.id'), unique=False, nullable=False)
+    department = relationship(Department)
+    fk_city = db.Column(db.Integer, ForeignKey('city.id'), unique=False, nullable=False)
+    city = relationship(City)
  
     @validates('type_identification')
     def validate_type_identification(self, key, value):
@@ -32,37 +70,4 @@ class User(db.Model):
         return value
 
     def __repr__(self):
-        return '<User {}>'.format(self.id)
-
-class Country(db.Model):
-    __tablename__ = "country"
-    id = db.Column(db.Integer, nullable=False)
-    name = db.Column(db.String(50), unique=False, nullable=False)
-    zip_code = db.Column(db.String(50), primary_key=True)
-
-    def __repr__(self):
-        return '<Country {}>'.format(self.id)
-    
-class Department(db.Model):
-    __tablename__ = "department"
-    id = db.Column(db.Integer, nullable=False)
-    name = db.Column(db.String(50), unique=False, nullable=False)
-    zip_code = db.Column(db.String(50), unique=True, nullable=False)
-    fk_country = db.Column(db.String(100), db.ForeignKey('country.zip_code'), unique=False, nullable=False)
-
-    __table_args__ = (
-        db.PrimaryKeyConstraint('fk_country', 'zip_code'),
-        {},
-    )
-
-    def __repr__(self):
-        return '<Department {}>'.format(self.id)
-    
-class City(db.Model):
-    __tablename__ = "city"
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), unique=False, nullable=False)
-    zip_code = db.Column(db.String(50), unique=True, nullable=False)
-
-    def __repr__(self):
-        return '<City {}>'.format(self.id)
+        return '<Farmer {}>'.format(self.id)
