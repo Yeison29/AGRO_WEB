95d6af622b9aa0c8aa9b938cb3b119ef6e8a5129
Yeison Ascanio <ydascanioa@ufpso.edu.co>
2023-05-15 20:33:37 -0500

    Se cambio a gender en el user.sql

--- user.sql
+++ user.sql
@@ -1,2 +1,2 @@
-INSERT INTO "user" (id, username, lastname, email, password, date_of_birth, type_identification, number_identification, genre, phone, address, type_user, fk_country, fk_department, fk_city) VALUES 
+INSERT INTO "user" (id, username, lastname, email, password, date_of_birth, type_identification, number_identification, gender, phone, address, type_user, fk_country, fk_department, fk_city) VALUES 
 (1,'Yeison','Ascanio','ydascanioa@ufpso.edu.co','ascanio','2001/09/29','C.C','1005074695','M','3122092826','Vereda Llano del pozo', 'S', 1, 18, 786);
