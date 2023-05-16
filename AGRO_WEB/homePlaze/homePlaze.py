fc60cd1b69cfa97f395e8638dcdbb27719993ba0
Josue0625 <jcampos@ufpso.edu.co>
2023-05-15 20:25:16 -0500

    Se cambio el navbar de agricultor y de plaza  y tambien se agrego el menu hecho por yeison, ademas se corrigieron si los datos de la tabla user iban a ser nulos y unicos

--- AGRO_WEB/homePlaze/homePlaze.py
+++ AGRO_WEB/homePlaze/homePlaze.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, render_template, url_for, redirect, request,jsonify,flash
-
+from ..models.models import User
 homePlaze_bp = Blueprint("homePlaze_bp", __name__, template_folder="templates", static_folder="static")
 
 @homePlaze_bp.route('/homePlaze')
 def homePlaze():
-
-    return render_template("homePlaze.html")
+    user = User.query.filter_by(email='ydascanioa@ufpso.edu.co',password='ascanio').first()
+    return render_template("homePlaze.html",user=user)
