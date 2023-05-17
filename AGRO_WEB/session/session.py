5f9c8b335d7272a4b48cdbf8a629205330044fba
Josue0625 <jcampos@ufpso.edu.co>
2023-05-17 09:31:23 -0500

    filtre por otro usuario diferente al superusuario yeison

--- AGRO_WEB/session/session.py
+++ AGRO_WEB/session/session.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, url_for, redirect, request,jsonify
 from ..models.models import User
 
 def getSessionUser():
-    # user = User.query.filter_by(email='ydascanioa@ufpso.edu.co',password='ascanio').first()
-    user = User.query.filter_by(email='corabastos@gamil.com',password='corabastos').first()
+    user = User.query.filter_by(email='ydascanioa@ufpso.edu.co',password='ascanio').first()
+    #user = User.query.filter_by(email='corabastos@gamil.com',password='corabastos').first()
     print(user)
     return user
